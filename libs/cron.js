74cf43268fd617ec00622f094d68d53e3a656249
daud <daudcanugerah@gmail.com>
2018-12-26 11:09:03 +0700

    fix(cron): fix error typo cron

--- libs/cron.js
+++ libs/cron.js
@@ -1,103 +1,99 @@
-const {getCronFormat} = require('./../libs/helper');
+const { getCronFormat } = require('./../libs/helper');
 const Model = require('./../model/model');
 const CronJob = require('cron').CronJob;
-const {isset} = require('./helper');
-const Engine =require('./engine');
+const { isset } = require('./helper');
+const Engine = require('./engine');
+
 const DB = new Model();
 
-var activeCron = {};
+const activeCron = {};
 
-const runCron = async function () { 
-  let cron = {};
-  let dbRequest = await DB.find({ collection: 'schedule', args: [{}] });
-  let tasks = await dbRequest.toArray();
-  if(tasks.length > 0){
+const runCron = async () => {
+  const cron = {};
+  const dbRequest = await DB.find({ collection: 'schedule', args: [{}] });
+  const tasks = await dbRequest.toArray();
+  if (tasks.length > 0) {
   // convert cron
-  for (let i = 0; i < tasks.length; i++) {
-    tasks[i]["time"] = getCronFormat(tasks[i].time);
-}
+    for (let i = 0; i < tasks.length; i++) {
+      tasks[i].time = getCronFormat(tasks[i].time);
+    }
 
-  tasks.forEach(item => {
-      activeCron[item._id] = (() => {
-      return new CronJob(item.time,async function () {
-        console.log('cron job runnint '+ Date())
-          let engine = new Engine(item);
-          engine.runEngine();
-        });
-      })();
-  });
-  return cron;
+    tasks.forEach((item) => {
+      activeCron[item._id] = (() => new CronJob(item.time, (async () => {
+        console.log(`cron job runnint ${Date()}`);
+        const engine = new Engine(item);
+        engine.runEngine();
+      })))();
+    });
+    return cron;
+  }
 };
 
-let startCron = function (id = null) {
-    if (isset(id)) {
-        try {
-            if (isset(activeCron[id])) {
-                activeCron[id].start();
-                return true;
-            } else {
-                return false;
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    } else {
-        try {
-            for (const key in activeCron) {
-                activeCron[key].start();
-            }
-            return true;
-        } catch(err){
-            throw err;
-        }
-    }
-}
-
-let stopCron = function (id = null) {
-    if (isset(id)) {
-        try {
-            if (isset(activeCron[id])) {
-                activeCron[id].stop();
-                return true;
-            } else {
-                return false;
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    } else {
-        try {
-            for (const key in activeCron) {
-                activeCron[key].stop();
-            }
-            return true;
-        } catch {
-            return false;
-        }
-    }
-}
-let getNexDate = (id, priode = 1) => {
+const startCron = (id = null) => {
+  if (isset(id)) {
     try {
-        if (isset(activeCron[id])) {
-            let data = activeCron[id].nextDates(1);
-            return data[0]._d;
-        } else {
-            return false;
-        }
+      if (isset(activeCron[id])) {
+        activeCron[id].start();
+        return true;
+      }
+      return false;
     } catch (err) {
-        console.log(err);
+      console.log(err);
     }
-}
-let getInstance = (id) => {
+  } else {
     try {
-        if (isset(activeCron[id])) {
-            return activeCron[id];
-        } else {
-            return false;
-        }
+      for (const key in activeCron) {
+        activeCron[key].start();
+      }
+      return true;
     } catch (err) {
-        console.log(err);
+      throw err;
     }
-}
+  }
+};
 
-module.exports = { runCron,getNexDate,getInstance };
+const stopCron =  (id = null) => {
+  if (isset(id)) {
+    try {
+      if (isset(activeCron[id])) {
+        activeCron[id].stop();
+        return true;
+      }
+      return false;
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    try {
+      for (const key in activeCron) {
+        activeCron[key].stop();
+      }
+      return true;
+    } catch (err) {
+      return ferr;
+    }
+  }
+};
+const getNexDate = (id, priode = 1) => {
+  try {
+    if (isset(activeCron[id])) {
+      const data = activeCron[id].nextDates(1);
+      return data[0]._d;
+    }
+    return false;
+  } catch (err) {
+    console.log(err);
+  }
+};
+const getInstance = (id) => {
+  try {
+    if (isset(activeCron[id])) {
+      return activeCron[id];
+    }
+    return false;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = { runCron, getNexDate, getInstance };
