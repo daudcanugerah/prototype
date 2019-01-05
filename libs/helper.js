b27cf91ccbc112b2aec45284aa99f5ce2b712bd1
daudcanugerah <daudcanugerah@gmail.com>
2019-01-05 07:43:14 +0700

    perf(Helper): delete mix function

--- libs/helper.js
+++ libs/helper.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 const multer = require('multer');
 
 const isset = function isset(v) {
@@ -39,6 +41,7 @@ const extractForm = function (data) {
   return newa;
 };
 
+/* eslint-disable */
 const converToCron = function (data, name) {
   if (isset(data[name])) {
     let result = '';
@@ -75,27 +78,6 @@ const getCronFormat = (data, layout = ['minute', 'hour', 'dayOfMonth', 'month', 
   return result.trimRight();
 };
 
-const mix = function (...mixins) {
-  class Mix { }
-
-  // Programmatically add all the methods and accessors
-  // of the mixins to class Mix.
-  for (const mixin of mixins) {
-    copyProperties(Mix, mixin);
-    copyProperties(Mix.prototype, mixin.prototype);
-  }
-
-  return Mix;
-};
-
-function copyProperties(target, source) {
-  for (const key of Reflect.ownKeys(source)) {
-    if (key !== 'constructor' && key !== 'prototype' && key !== 'name') {
-      const desc = Object.getOwnPropertyDescriptor(source, key);
-      Object.defineProperty(target, key, desc);
-    }
-  }
-}
 module.exports = {
-  isset, upload, imageFilter, extractForm, getCronFormat, converToCron, mix,
+  isset, upload, imageFilter, extractForm, getCronFormat, converToCron,
 };
