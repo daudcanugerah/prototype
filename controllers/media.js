dc54510e17dd53477a98833f8fe81d959cdceb73
daudcanugerah <daudcanugerah@gmail.com>
2019-01-06 11:09:15 +0700

    feat(Post): make upload friendly

--- controllers/media.js
+++ controllers/media.js
@@ -3,19 +3,23 @@ const Model = require('./../model/mediaModel').getInstance();
 
 class Media {
   upload() {
-    return (req, res) => {
+    return async (req, res) => {
       // check validation error by express-validator
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
+        const errorFormatter = ({
+          location, msg, param, value, nestedErrors,
+        }) => `${msg}`;
+        const result = validationResult(req).formatWith(errorFormatter);
+        return res.status(422).json({ 'jquery-upload-file-error': result.array()[0] });
       }
       // check file upload
       const { files } = req;
       if (!files) {
         res.sendStatus(422).json('error uploading file');
       }
-      Model.addMedia({ detailMedia: files[0] });
-      return res.sendStatus(200);
+      const addMedia = await Model.addMedia({ detailMedia: files[0] });
+      return res.json({ id: addMedia[0]._id.toString() });
     };
   }
 }
